de8c8fde7cf39b8a32781b16005714fd1f5e3754
iokanaan <iokannaan@hotmail.com>
2025-08-14 19:34:24 -0300

    a

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from flask import Flask, render_template, request, redirect, url_for
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-def read_root():
-    return {"message": "HELLO MI HERMANITO, SE TU LER ISSO TU É GAY."}
+# Rota para a página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    if request.method == 'POST':
+        valor_do_input = request.form.get('meu_input')
+
+        print(f"O usuário digitou: {valor_do_input}")
+
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
