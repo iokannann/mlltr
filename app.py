03e282b68bb78b276c11a73009cdfd287a39614b
iokanaan <iokannaan@hotmail.com>
2025-09-08 20:12:37 -0300

    2° tela

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def enviar():
              NOME_SECRETO = os.environ.get('NOME_SECRETO', 'ikn')
              if valor_do_input == NOME_SECRETO:
                   return redirect(url_for('secret'))
+             if valor_do_input == '':
+                  return redirect(url_for('second'))
              else:
                   conn = get_db_connection()
                   conn.execute('INSERT INTO dados (valor) VALUES (?)', (valor_do_input,))
